332b25831b8989153a92367c2cd8548a126d0b13
Dave Arnold <darnold@hashicorp.com>
2018-06-20 16:30:38 -0700

    trying to launch consul cluster

--- variables.tf
+++ variables.tf
@@ -1,5 +1,4 @@
 variable "consul_cluster_key_name" {}
 variable "consul_cluster_servers" {}
-variable "consul_cluster_service_conf_dest" {}
 variable "consul_cluster_ssh_private_key" {}
 variable "consul_cluster_tagName" {}
