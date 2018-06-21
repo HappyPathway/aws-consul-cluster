ac01ceff9c1f16796b7ca3ead0259c6a9b11b969
Dave Arnold <darnold@hashicorp.com>
2018-06-20 22:16:22 -0700

    Setting A record instead of CNAME

--- dns.tf
+++ dns.tf
@@ -7,7 +7,7 @@ resource "aws_route53_record" "consul" {
   count   = "${var.set_dns ? 1 : 0}"
   zone_id = "${data.aws_route53_zone.selected.zone_id}"
   name    = "${var.host_name}.${var.domain}"
-  type    = "CNAME"
+  type    = "A"
   ttl     = "300"
   records = ["${module.consul_cluster.public_addresses}"]
 }
