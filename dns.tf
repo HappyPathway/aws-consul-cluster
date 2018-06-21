2d352a5f7278c98d21001d5c641f96d558060001
Dave Arnold <darnold@hashicorp.com>
2018-06-20 22:01:03 -0700

    adding dns

---
+++ dns.tf
@@ -0,0 +1,13 @@
+data "aws_route53_zone" "selected" {
+  count = "${var.set_dns ? 1 : 0}"
+  name  = "${var.domain}"
+}
+
+resource "aws_route53_record" "consul" {
+  count   = "${var.set_dns ? 1 : 0}"
+  zone_id = "${data.aws_route53_zone.selected.zone_id}"
+  name    = "${var.host_name}.${var.domain}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${module.consul_cluster.public_addresses}"]
+}
--- variables.tf
+++ variables.tf
@@ -4,3 +4,13 @@ variable "consul_cluster_ssh_private_key" {}
 variable "consul_cluster_tagName" {}
 variable "organization" {}
 variable "workspace" {}
+
+variable "set_dns" {
+  default = false
+}
+
+variable "host_name" {
+  default = "consul"
+}
+
+variable "domain" {}
