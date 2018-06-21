fc1fab58ea9f3061aea96a5b11826e4562d8dde7
Dave Arnold <darnold@hashicorp.com>
2018-06-21 09:37:57 -0700

    trying to install beta version with connect configuration

--- main.tf
+++ main.tf
@@ -17,12 +17,14 @@ provider "aws" {
 //--------------------------------------------------------------------
 // Modules
 module "consul_cluster" {
-  source          = "app.terraform.io/Darnold-Hashicorp/consul-cluster/aws"
-  version         = "1.0.5"
-  key_name        = "${var.consul_cluster_key_name}"
-  servers         = "${var.consul_cluster_servers}"
-  ssh_private_key = "${var.consul_cluster_ssh_private_key}"
-  subnet          = "${data.terraform_remote_state.network.public_subnet}"
-  tagName         = "${var.consul_cluster_tagName}"
-  vpc_id          = "${data.terraform_remote_state.network.vpc_id}"
+  source              = "app.terraform.io/Darnold-Hashicorp/consul-cluster/aws"
+  version             = "1.0.7"
+  key_name            = "${var.consul_cluster_key_name}"
+  servers             = "${var.consul_cluster_servers}"
+  ssh_private_key     = "${var.consul_cluster_ssh_private_key}"
+  subnet              = "${data.terraform_remote_state.network.public_subnet}"
+  tagName             = "${var.consul_cluster_tagName}"
+  vpc_id              = "${data.terraform_remote_state.network.vpc_id}"
+  consul_config       = "${path.root}/consul_config.json"
+  consul_download_url = "https://s3.us-east-2.amazonaws.com/consul-dev-artifacts/consul-connect/1.2.0-beta4/consul_1.2.0-beta4_linux_amd64.zip"
 }
