5ee6f831a183eb404c287a3b95fdb3be5d941b75
Dave Arnold <darnold@hashicorp.com>
2018-06-25 08:11:30 -0700

    removing unused variables

--- main.tf
+++ main.tf
@@ -31,17 +31,13 @@ data "aws_subnet" "selected" {
 //--------------------------------------------------------------------
 // Modules
 module "consul_cluster" {
-  source              = "app.terraform.io/Darnold-Hashicorp/consul-cluster/aws"
-  version             = "1.3.2"
-  key_name            = "${var.consul_cluster_key_name}"
-  servers             = "${var.consul_cluster_servers}"
-  ssh_private_key     = "${var.consul_cluster_ssh_private_key}"
-  subnet              = "${data.terraform_remote_state.network.public_subnet}"
-  tagName             = "${var.consul_cluster_tagName}"
-  vpc_id              = "${data.terraform_remote_state.network.vpc_id}"
-  consul_config       = "${path.root}/consul_config.json"
-  consul_download_url = "${var.consul_download_url}"
-  resource_tags       = "${var.resource_tags}"
-  availability_zone   = "${data.aws_subnet.selected.availability_zone}"
-  env                 = "${var.env}"
+  source            = "app.terraform.io/Darnold-Hashicorp/consul-cluster/aws"
+  version           = "1.3.3"
+  key_name          = "${var.consul_cluster_key_name}"
+  servers           = "${var.consul_cluster_servers}"
+  subnet            = "${data.terraform_remote_state.network.public_subnet}"
+  vpc_id            = "${data.terraform_remote_state.network.vpc_id}"
+  resource_tags     = "${var.resource_tags}"
+  availability_zone = "${data.aws_subnet.selected.availability_zone}"
+  env               = "${var.env}"
 }
