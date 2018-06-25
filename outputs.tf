6caa559c38de3973acd4a6189f8df1bf323b20fe
Dave Arnold <darnold@hashicorp.com>
2018-06-25 07:55:03 -0700

    converting to asg

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "cluster_sg" {
   value = "${module.consul_cluster.security_group}"
 }
 
-output "instances" {
-  value = "${module.consul_cluster.instances}"
+output "cluster" {
+  value = "${module.consul_cluster.cluster}"
 }
